f8d86b1863b86d9d9db65e57e813469f45a3c3d5
Victor <robertovictor0702@gmail.com>
2022-12-20 16:44:01 -0300

    JSX arrays 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,22 @@
 import React from "react";
 
-// Mostre os dados da aplicação, como aprensetado no vídeo
-// Não utilize CSS externo, use o style para mudar as cores
-// Se a situação estiver ativa pinte de verde, inativa vermelho
-// Se o gasto for maior que 10000 mostre uma mensagem
-const luana = {
-  cliente: 'Luana',
-  idade: 27,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-  ],
-  ativa: true,
-};
-
-const mario = {
-  cliente: 'Mario',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-    { nome: 'Guitarra', preco: 'R$ 3500' },
-  ],
-  ativa: false,
-};
-
-const totalGasto = (dados) => {
-  const preco = dados.compras
-  const precoMap = preco.map(valor => Number(valor.preco.replace('R$ ', ''))).reduce((acumulador, item) => acumulador + item);
-  
-  return precoMap
-}
-
 const App = () => {
-  const dados = mario;
-
-  return <div>
-    <p>Nome: {dados.cliente}</p>
-    <p>Idade: {dados.idade} </p>
-    <p>Situação: 
-     <span style={{ color: dados.ativa ? 'green' : 'red'}}> {dados.ativa ? 'ativa' : 'inativa'} </span>
-    </p>
-    
-    <p>Total gasto: R${totalGasto(dados)}</p>
-    <p>{totalGasto(dados) > 10000 && 'você está gastando demais'}</p>
-  </div>;
+  const livros = [
+    { nome: "A Game of Thrones", ano: 1996 },
+    { nome: "A Clash of Kings", ano: 1998 },
+    { nome: "A Storm of Swords", ano: 2000 },
+  ];
+  return (
+    <ul>
+      {livros
+        .filter(({ ano }) => ano >= 1998)
+        .map(({ nome, ano }) => (
+          <li key={nome}>
+            {nome}, {ano}
+          </li>
+        ))}
+    </ul>
+  );
 };
 
 export default App;
